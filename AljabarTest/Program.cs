76a581f5af9b4ee06ac38fab89a6e996294064c8
Sheilaa5 <chantikasheila105@gmail.com>
2025-05-04 10:25:37 +0700

    Add project files.

---
+++ AljabarTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LibraryAljabar;
+
+namespace AljabarTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] persamaanKuadrat = { 1, -3, -10 };
+            double[] akar = PersamaanKuadrat.AkarPersamaanKuadrat(persamaanKuadrat);
+            Console.WriteLine("Akar-akar: " + string.Join(", ", akar));
+
+            double[] bentukKuadrat = { 2, -3 };
+            double[] hasil = PersamaanKuadrat.HasilKuadrat(bentukKuadrat);
+            Console.WriteLine("Hasil kuadrat: " + string.Join(", ", hasil));
+        }
+    }
+}
+
---
+++ LibraryAljabar/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LibraryAljabar
+{
+    public class PersamaanKuadrat
+    {
+        
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+                throw new ArgumentException("Persamaan tidak memiliki akar real.");
+            }
+
+            double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+            return new double[] { x1, x2 };
+        }
+
+        
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double a2 = a * a;
+            double ab2 = 2 * a * b;
+            double b2 = b * b;
+
+            return new double[] { a2, ab2, b2 };
+        }
+    }
+}
